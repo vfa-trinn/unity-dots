3eea7f05bd6ca8ee658d4596ea1588d3d8dc599c
Tri Ngo <vfa.trinn@gmail.com>
2020-09-07 23:15:22 +0700

    add: demo scene with DOTS

---
+++ Sample-DOTS/Assets/Scripts/System/MoveSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class MoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+         
+        JobHandle jobHandle = Entities.ForEach((ref Translation translation, ref MoveSpeedComponent moveSpeed) => {
+            translation.Value.x += moveSpeed.value * deltaTime;
+            if (translation.Value.x > 5) moveSpeed.value = -math.abs(moveSpeed.value);
+            if(translation.Value.x < -5) moveSpeed.value = math.abs(moveSpeed.value);
+        }).Schedule(inputDeps);
+
+        return jobHandle;
+    }
+}
