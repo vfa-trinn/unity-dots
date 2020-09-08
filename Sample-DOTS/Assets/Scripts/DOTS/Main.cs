43c81c842942d1b6ef9757620c1e9f5dfd238b02
Tri Ngo <vfa.trinn@gmail.com>
2020-09-08 08:48:02 +0700

    add: normal scene

---
+++ Sample-DOTS/Assets/Scripts/DOTS/Main.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+namespace DOTS
+{
+    public class Main : MonoBehaviour
+    {
+        public int playerNumber = 10000;
+
+        void Start()
+        {
+            Unity.Mathematics.Random rand = new Unity.Mathematics.Random(111);
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            Entity prefabEntity = entityManager.CreateEntityQuery(typeof(PrefabEntityComponent)).GetSingleton<PrefabEntityComponent>().prefabEnity;
+
+            for (int i = 0; i < playerNumber; i++)
+            {
+                var instance = entityManager.Instantiate(prefabEntity);
+                entityManager.SetComponentData(instance, new Translation { Value = new float3(rand.NextFloat(-5, 5), rand.NextFloat(-5, 5), 0) });
+                entityManager.SetComponentData(instance, new MoveSpeedComponent { value = rand.NextFloat(1, 3) });
+            }
+        }
+    }
+}
---
+++ Sample-DOTS/Assets/Scripts/WithoutDOTS/Main.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WithoutDOTS
+{
+    public class Main : MonoBehaviour
+    {
+        public int playerNumber = 10000;
+        public GameObject playerPrefab;
+
+        List<PlayerData> playerTransforms = new List<PlayerData>();
+
+        void Start()
+        {
+            System.Random rand = new System.Random(111);
+
+            for (int i = 0; i < playerNumber; i++)
+            {
+                var instance = Instantiate(playerPrefab).AddComponent<PlayerData>();
+                playerTransforms.Add(instance);
+
+                instance.transform.position = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0);
+                instance.moveSpeed = Random.Range(1f, 3f);
+            }
+        }
+
+        private void Update()
+        {
+            for (int i = 0, l  = playerTransforms.Count; i < l; i++)
+            {
+                var player = playerTransforms[i];
+                player.transform.position += new Vector3(player.moveSpeed * Time.deltaTime, 0, 0);
+                if (player.transform.position.x > 5) player.moveSpeed = -Mathf.Abs(player.moveSpeed);
+                if (player.transform.position.x < -5) player.moveSpeed = Mathf.Abs(player.moveSpeed);
+            }
+        }
+    }
+}
